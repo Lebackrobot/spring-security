37df6c9d7c02f0c3447da06ca4dee41632add59a
Pedro Fernandes <49316490+Lebackrobot@users.noreply.github.com>
2024-10-02 23:13:06 +0000

    init: restart project

--- src/main/java/com/example/spring_security/repositories/UserRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_security.repositories;
-
-import com.example.spring_security.models.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepositories extends JpaRepository<UserModel, Long> {
-}
